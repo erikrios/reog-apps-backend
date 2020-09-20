3ff1bae3ac445cf26cbdce7f8af587021cf06dec
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-20 19:49:08 +0700

    Create database connection file

---
+++ startup/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+module.exports = function () {
+    const connectionString = config.get('db');
+
+    mongoose.connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
+        .then(() => console.log('Connected to database...'))
+        .catch(ex => console.log(ex));
+}
