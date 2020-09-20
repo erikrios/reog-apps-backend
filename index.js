574a42fcd6825d885f66126d6fdb62deab3cecc9
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-20 19:43:47 +0700

    Import production module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 require('./startup/routes')(app);
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => { console.log(`Server listening on port ${port}...`) });
