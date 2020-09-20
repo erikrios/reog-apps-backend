438d80f8b0e9ccacce045b44d401e42c37f558db
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-20 19:34:34 +0700

    Create home.js

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const config = require('config');
+const Response = require('../models/response');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     const message = config.get('message');
+    
+    res.send(new Response(
+        'success',
+        [
+            { message: message }
+        ],
+        null
+    ));
 })
 
 module.exports = router;
