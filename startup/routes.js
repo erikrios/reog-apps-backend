cc0629795dbbe12c02eb3a00f99d52f8c7f000d5
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-20 20:20:36 +0700

    Import useragent modules

--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const useragent = require('express-useragent');
 
 const home = require('../routes/home');
 
 module.exports = function (app) {
     app.use(express.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(useragent.express());
     app.use('/', home);
 }
