fbc010cb11408a9b0e320577f801eb676c9ea397
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-20 20:38:34 +0700

    Refactor the code

--- middleware/useragent.js
+++ middleware/useragent.js
@@ -1,4 +1,17 @@
-module.exports = function(req, res, next) {
-    console.log(req.useragent);
+module.exports = function (req, res, next) {
+    const { isMobile, isDesktop, isBot, browser, version, os, platform, source } = req.useragent;
+    console.log({
+        useragent: {
+            isMobile,
+            isDesktop,
+            isBot,
+            browser,
+            version,
+            os,
+            platform,
+            source
+        }
+    });
+    
     next();
 };
