f3d9a063312b008799dce4de4ba7ca7df7e29750
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-20 19:39:52 +0700

    Import routes module

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 const express = require('express');
-const config = require('config');
-
 const app = express();
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    const message = config.get('message');
-
-    res.send({
-        status: 'success',
-        data: [
-            {
-                message: message
-            }
-        ],
-        message: null
-    });
-});
+require('./startup/routes')(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => { console.log(`Server listening on port ${port}...`) });
