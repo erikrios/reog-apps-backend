98ca444ebabb892da46f9a63acab73cb69ba4821
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-20 20:26:38 +0700

    User useragent middleware

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const config = require('config');
 const Response = require('../models/response');
+const useragent = require('../middleware/useragent');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', useragent, (req, res) => {
     const message = config.get('message');
     
     res.send(new Response(
