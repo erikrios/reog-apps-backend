fb5f51d1b3f6fe50f1d4082347a5412bed0ce762
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-21 10:23:46 +0700

    Add image upload with multer

--- startup/routes.js
+++ startup/routes.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 const useragent = require('express-useragent');
 
 const home = require('../routes/home');
+const image = require('../routes/image');
 
 module.exports = function (app) {
     app.use(express.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(useragent.express());
     app.use('/', home);
+    app.use('/api/image', image);
 };
