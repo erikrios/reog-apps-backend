5c7a0ec3f88492ccf24054ce2a85efad963613ca
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-21 19:25:54 +0700

    Bugs fixing

--- routes/news.js
+++ routes/news.js
@@ -37,10 +37,16 @@ router.get('/:id', async (req, res) => {
     const id = req.params.id;
 
     try {
-        const news = await News.findOne({ _id: id })
+        const news = await News
+            .findOneAndUpdate({ _id: id }, {
+                $inc: {
+                    views: 1
+                }
+            }, { new: true })
             .select('-__v');
 
         if (!news) return res.status(404).send(new Response('error', null, 'News with given id was not found.'))
+
         res.send(new Response('success', [news], null));
     } catch (err) {
         res.status(500).send(new Response('error', null, err.message));
@@ -61,6 +67,29 @@ router.post('/', auth, async (req, res) => {
 
         await news.save();
         res.send(send(new Response('success', [news], null)));
+    } catch (err) {
+        res.status(500).send(new Response('error', null, err.message));
+    }
+});
+
+router.put('/:id', auth, (req, res) => {
+    const { error } = req.body;
+    if (error) return res.status(400).send(new Response('error', null, error.details[0].message));
+
+    if (!req.user.isAdmin) return res.status(401).send(new Response('error', null, 'Access denied for non-admin.'));
+
+    const id = req.params.id;
+
+    try {
+        const news = await News.findOneAndUpdate({ _id: id }, {
+            $set: {
+                'title': req.body.title,
+                'description': req.body.description
+            }
+        }, { new: true });
+
+        if (!news) return res.status(404).send(new Response('error', null, 'News with given id was not found.'));
+        res.send(new Response('success', [news], null));
     } catch (err) {
         res.status(500).send(new Response('error', null, err.message));
     }
