a3da99c9a3967a36f9403fa5ac6bf3a716f9d7b2
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-21 12:07:36 +0700

    Import the routes

--- startup/routes.js
+++ startup/routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const home = require('../routes/home');
+const users = require('../routes/users');
+const auth = require('../routes/auth');
 const image = require('../routes/image');
 
 module.exports = function (app) {
     app.use(express.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use('/', home);
+    app.use('/users', users);
+    app.use('/auth', auth);
     app.use('/api/image', image);
 };
