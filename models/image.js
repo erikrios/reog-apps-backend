1891e254736e9337664165d501e635de57b8f5c0
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-22 12:00:47 +0700

    Create image model class

---
+++ models/image.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const imageSchema = new mongoose.Schema({
+    image: {
+        type: Buffer,
+        required: true
+    },
+    article: {
+        type: new mongoose.Schema({
+            title: {
+                type: String,
+                minlength: 1,
+                maxlength: 255,
+                required: true
+            }
+        }),
+        required: true
+    }
+});
+
+const Image = mongoose.model('Image', imageSchema);
+
+module.exports = Image;
