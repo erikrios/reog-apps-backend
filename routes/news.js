73781fafe8b37230728aa1e435bdfa3e88b6420f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-22 13:48:44 +0700

    Add post, update, and delete image routes

--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { News, validate } = require('../models/news');
+const Image = require('../models/image');
 const Response = require('../models/response');
 const auth = require('../middleware/auth');
+const avatar = require('../middleware/avatar');
 
 const router = express.Router();
 
@@ -111,6 +113,61 @@ router.delete('/:id', auth, async (req, res) => {
 
         if (!news) return res.status(404).send(new Response('error', null, 'News with given id was not found.'));
         res.send(new Response('success', [news], null));
+    } catch (err) {
+        res.status(500).send(new Response('error', null, err.message));
+    }
+});
+
+router.post('/image', [auth, avatar], async (req, res) => {
+    if (!req.user.isAdmin) return res.status(401).send(new Response('error', null, 'Access denied for non-admin.'));
+
+    const id = req.query.id
+
+    try {
+        const newsCount = await News.count({ _id: id });
+        if (newsCount < 1) return res.status(404).send(new Response('error', null, 'News with given id was not found.'));
+
+        const image = new Image({
+            image: req.file.buffer,
+            article: id
+        });
+
+        await image.save();
+        await News.updateOne({ _id: id }, { $push: { images: image._id } });
+
+        res.send(new Response('success', null, null));
+    } catch (err) {
+        res.status(500).send(new Response('error', null, err.message));
+    }
+});
+
+router.put('/image/:id', [auth, avatar], async (req, res) => {
+    if (!req.user.isAdmin) return res.status(401).send(new Response('error', null, 'Access denied for non-admin.'));
+
+    const id = req.params.id;
+
+    try {
+        const imageCount = await Image.count({ _id: id });
+        if (imageCount < 1) return res.status(404).send(new Response('error', null, 'Image with given id was not found.'));
+
+        await Image.updateOne({ _id: id }, { $set: { image: req.file.buffer } });
+        res.send(new Response('success', null, null));
+    } catch (err) {
+        res.status(500).send(new Response('error', null, err.message));
+    }
+});
+
+router.delete('/image/:id', [auth, avatar], async (req, res) => {
+    if (!req.user.isAdmin) return res.status(401).send(new Response('error', null, 'Access denied for non-admin.'));
+
+    const id = req.params.id
+
+    try {
+        const imageCount = await Image.count({ _id: id });
+        if (imageCount < 1) return res.status(404).send(new Response('error', null, 'Image with given id was not found.'));
+
+        await Image.deleteOne({ _id: id });
+        res.send(new Response('success', null, null));
     } catch (err) {
         res.status(500).send(new Response('error', null, err.message));
     }
