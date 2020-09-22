3c81655d913f8065cc75b1cc67aaf6052f320b50
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-22 18:08:39 +0700

    Create comment model

---
+++ models/comment.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const commentSchema = new mongoose.Schema({
+    comment: {
+        type: String,
+        minlength: 1,
+        maxlength: 1024,
+        required: true
+    },
+    user: {
+        type: new mongoose.Schema({
+            name: {
+                type: String,
+                minlength: 5,
+                maxlength: 50,
+                required: true
+            }
+        }),
+        required: true
+    },
+    article: {
+        type: new mongoose.Schema({
+            title: {
+                type: String,
+                minlength: 1,
+                maxlength: 255,
+                required: true
+            }
+        }),
+        required: true
+    }
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+function validateComment(comment) {
+    const schema = Joi.object({
+        comment: Joi.string().min(1).max(1024).required()
+    });
+
+    return schema.validate(comment);
+}
+
+module.exports = {
+    Comment: Comment,
+    validate: validateComment
+};
