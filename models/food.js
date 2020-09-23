45700ce7fe130fee3a24725be2efbd6cceaf82a5
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-23 12:34:50 +0700

    Create food model class

---
+++ models/food.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const Schema = mongoose.Schema
+
+const foodSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        minlength: 1,
+        maxlength: 255,
+        required: true
+    },
+    description: {
+        type: String,
+        minlength: 1,
+        maxlength: 102400,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    images: [{ type: Schema.Types.ObjectId, ref: 'Image' }]
+});
+
+const Food = mongoose.model('Food', foodSchema);
+
+function validateFood(site) {
+    const schema = Joi.object({
+        title: Joi.string().min(1).max(255).required(),
+        description: Joi.string().min(1).max(102400).required()
+    });
+
+    return schema.validate(site);
+}
+
+module.exports = {
+    Food: Food,
+    validate: validateFood
+};
