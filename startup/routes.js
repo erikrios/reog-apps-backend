e6087e11391b0d2c84a5ff49234489d8638b4a01
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-31 02:03:23 +0700

    Use cors middleware

--- startup/routes.js
+++ startup/routes.js
@@ -15,7 +15,7 @@ const comments = require('../routes/comments');
 module.exports = function (app) {
     app.use(express.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(cors);
+    app.use(cors());
     app.use('/', home);
     app.use('/api/users', users);
     app.use('/api/auth', auth);
